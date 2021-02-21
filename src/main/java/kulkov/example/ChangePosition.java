eddf54054f76a3a58d0f73e449ae988e2c76d739
Garry Deepcool <garrydeepcool@outlook.com>
2021-02-22 00:25:03 +0500

    Initial commit.

--- src/main/java/kulkov/example/ChangePosition.java
+++ src/main/java/kulkov/example/ChangePosition.java
@@ -6,14 +6,14 @@ import java.util.Arrays;
 public class ChangePosition<T> {
     private T[] array;
 
-    private void changePosition(T[] inputArray, int changeFrom, int changeTo) {
+    public void changePosition(T[] inputArray, int changeFrom, int changeTo) {
         T tempVar = inputArray[changeFrom];
         inputArray[changeFrom] = inputArray[changeFrom];
         inputArray[changeFrom] = tempVar;
 
     }
 
-    private ArrayList<T> convert(T[] inputArray)    {
+    public ArrayList<T> convert(T[] inputArray)    {
         ArrayList<T> arr = new ArrayList<>(Arrays.asList(inputArray));
         return arr;
     }
--- src/main/java/kulkov/example/Main.java
+++ src/main/java/kulkov/example/Main.java
@@ -4,6 +4,21 @@ import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
+        ChangePosition cp = new ChangePosition();
+
+        String[] strArr = {"dog", "cat", "frog", "cow", "owl"};
+
+        for (String str :
+                strArr) {
+            System.out.printf("[]: %s\n", str);
+        }
+
+        cp.changePosition(strArr, 1, 2);
+
+        for (String str :
+                strArr) {
+            System.out.printf("[]: %s\n", str);
+        }
 
     }
 }
